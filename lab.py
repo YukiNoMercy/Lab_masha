2ae47be6eb1e0710c65ce840940f63b590a0976a
YukiNoMercy <matveyschenikov@mail.ru>
2023-11-02 02:20:42 +0300

    working first task

--- lab.py
+++ lab.py
@@ -1,12 +1,25 @@
-text = open("16.txt", "r")
-count = 0
-maximum = 0
-max_i = 0
-for lines in text:
-    for i in range(len(lines)):
-        if lines[i] == " ":
-            if count > maximum:
-                maximum = count
-                max_i = i - count
-        count += 1
-text.close()
+def chanch_string():
+    text = open("16.txt", "r")
+    text_write = open("res.txt", "w+")
+    count = 0
+    maximum = 0
+    max_i = 0
+    for lines in text:
+        for i in range(len(lines)):
+            if lines[i] == " ":
+                if count > maximum:
+                    maximum = count
+                    max_i = i - count
+                count = 0
+            count += 1
+
+        for i in range(max_i, maximum):
+            if lines[i] == 'o':
+                lines = lines.replace('o', 'y')
+            if lines[i] == 'i':
+                lines = lines.replace('i', 'j')
+        text_write.write(lines)
+    text_write.close()
+    text.close()
+
+chanch_string()
