ab8d69018286377cbe900cab984bd171f8faca9f
YukiNoMercy <matveyschenikov@mail.ru>
2023-11-02 03:06:56 +0300

    rename first function and ready second function

--- lab.py
+++ lab.py
@@ -1,9 +1,10 @@
-def chanch_string():
+def change_file():
     text = open("16.txt", "r")
     text_write = open("res.txt", "w+")
     count = 0
     maximum = 0
     max_i = 0
+
     for lines in text:
         print(lines, end='')
         for i in range(len(lines)):
@@ -21,8 +22,30 @@ def chanch_string():
                 lines = lines.replace('i', 'j')
         print(lines, end='')
         text_write.write(lines)
-
+    print("\n")
     text_write.close()
     text.close()
 
-chanch_string()
+def numbers():
+    text = open("int16.txt", "r")
+    text_write = open("res.txt", "w+")
+
+    temp_array = []
+    for lines in text:
+        s = ""
+        print(lines, end='')
+        temp = lines.split()
+        for i in temp:
+            if int(i) % 2 == 0:
+                temp_array.append(int(i))
+    temp_array.sort(reverse=True)
+    for i in temp_array:
+        s += (str(i) + " ")
+    print("\n", s)
+    text_write.write(s)
+
+
+    text.close()
+    text_write.close()
+change_file() #Надо функции вызывать по очереди
+numbers()
